be76b25e005fe66fbdfab4fd43f4ab315cf13ca2
Mouhcine Essafi <messafi1337@gmail.com>
2023-12-25 14:15:40 +0100

    Update home_view.php

--- views/home_view.php
+++ views/home_view.php
@@ -5,19 +5,10 @@
         <th>Username</th>
     </tr>
     </thead>
-    <tbody>
 
-    <?php foreach ($users as $user) : ?>
-        <tr>
-            <td><?= $user['users_email'] ?></td>
-            <td><?= $user['users_username'] ?></td>
-        </tr>
-    <?php endforeach; ?>
-
-    </tbody>
 </table>
 
-<form action="index.php?page=home" method="POST">
+<form action="index.php?page=register" method="POST">
     <button type="submit" name="login">Login</button>
     <button type="submit" name="logout">Logout</button>
 </form>
