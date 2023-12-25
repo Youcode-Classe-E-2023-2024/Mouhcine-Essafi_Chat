90a8a8911a35e8c14bbe998e470fa9e1f6b586b3
Mouhcine Essafi <messafi1337@gmail.com>
2023-12-25 17:25:59 +0100

    Update login_controller.php

--- controllers/login_controller.php
+++ controllers/login_controller.php
@@ -3,9 +3,11 @@
 if (isset($_POST['submit_l'])) {
     $userRegistration = new UserLogin($db);
     $registrationResult = $userRegistration->loginUser($_POST['log_email'], $_POST['log_password']);
-    // echo "<script>alert('$registrationResult');</script>";
+    echo '<div class="alert">' . $registrationResult . '</div>';
     if ($registrationResult === true) {
-        header('location: index.php?page=register');
+        header('location: index.php?page=');
     }
+    else
+        header('location: index.php?page=login');
 }
 ?>
