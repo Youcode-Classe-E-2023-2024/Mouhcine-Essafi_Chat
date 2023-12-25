ac5b85c4cf5990214500fc4da65205832d4e1527
Mouhcine Essafi <messafi1337@gmail.com>
2023-12-25 14:15:37 +0100

    Update _layout.php

--- views/_layout.php
+++ views/_layout.php
@@ -10,7 +10,6 @@
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body>
-    <h1><?= ucfirst($page) ?> View</h1>
 
     <main>
         <?php include_once 'views/' . $page . '_view.php'; ?>
