4d237f289d87d6719549dd6c520b27bf194a5993
Mouhcine Essafi <messafi1337@gmail.com>
2023-12-25 12:31:39 +0100

    Update _layout.php

--- views/_layout.php
+++ views/_layout.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?= ucfirst($page) ?></title>
     <link rel="stylesheet" href="<?= PATH ?>assets/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body>
     <h1><?= ucfirst($page) ?> View</h1>
